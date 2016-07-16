87c7deb4b325d979f4260b82941e45c5f294a7fa
nicolasfazzari <nicolasfazzari@gmail.com>
2016-07-16 10:55:20 +0200

    design

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -80,6 +80,6 @@ class LocationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
-      params.require(:location).permit(:address, :zip, :city, :latitude, :longitude,)
+      params.require(:location).permit(:address, :zip, :city, :latitude, :longitude, :phone, :name)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626193043) do
+ActiveRecord::Schema.define(version: 20160716080333) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "address"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20160626193043) do
     t.datetime "updated_at", null: false
     t.integer  "zip"
     t.string   "city"
+    t.string   "name"
+    t.integer  "phone"
   end
 
   create_table "users", force: :cascade do |t|
